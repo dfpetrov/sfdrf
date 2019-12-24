f1042fd153fc441ca058b40c8ab248a6e7277525
Гараев Денис Флюрович <denis.garaev@OCRV.COM.RZD>
2019-12-24 11:26:07 +0300

    Init

--- blog/settings.py
+++ blog/settings.py
@@ -20,11 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-9uwduuf*-a84he=1$*p^tqd@*dz$4^+skbtyaav++&y(17y#q'
+# SECRET_KEY = '-9uwduuf*-a84he=1$*p^tqd@*dz$4^+skbtyaav++&y(17y#q'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '-9uwduuf*-a84he=1$*p^tqd@*dz$4^+skbtyaav++&y(17y#q')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+
 
 ALLOWED_HOSTS = ['*']
 
@@ -76,12 +79,25 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+import dj_database_url
+
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
